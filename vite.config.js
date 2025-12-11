4317a77cb24e389b3c4fe90949d9277b2cd833e2
ibkcomte <tolulopeibukun4@gmail.com>
2025-12-11 22:56:11 +0100

    feat: setup deployment for github pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+
+const repoName = 'SmartDesk';
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${SmartDesk}/`,
   plugins: [react(), tailwindcss()]
 })
